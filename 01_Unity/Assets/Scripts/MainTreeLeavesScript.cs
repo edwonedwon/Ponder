06025fa21d07f1661ce64e25e4441764d4c3faa5
Steven Edwon <edwon@me.com>
2012-09-06 17:52:22 -0700

    added MainTreeLeavesScript to MainTreeLeaves

---
+++ 01_Unity/Assets/Scripts/MainTreeLeavesScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class MainTreeLeavesScript : MonoBehaviour {
+	
+	private int attractionPower;
+
+	void Start () {
+		attractionPower = 10;
+	}
+	
+	void Update () {
+		
+	}
+}
