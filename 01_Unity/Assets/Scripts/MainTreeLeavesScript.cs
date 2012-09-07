69aaa76cb70daeeab96c040eab612a7d3bb9f2f1
Gilad Gray <giladgray@gmail.com>
2012-09-06 21:43:50 -0700

    add more logging to GUI

--- 01_Unity/Assets/Scripts/MainTreeLeavesScript.cs
+++ 01_Unity/Assets/Scripts/MainTreeLeavesScript.cs
@@ -16,6 +16,8 @@ public class MainTreeLeavesScript : MonoBehaviour {
 	}
 	
 	void OnGUI() {
-		GUI.Label(new Rect(10, 10, 100, 20), "Pollen: " + Mathf.RoundToInt(pollen));
+		GUI.Label (new Rect(10, 10, 100, 20), "Pollen: " + Mathf.RoundToInt(pollen));
+		GUI.Label (new Rect(10, 30, 100, 20), "Plants: " + GameObject.FindGameObjectsWithTag("plant").Length);
+		GUI.Label (new Rect(10, 50, 100, 20), "Insects: " + GameObject.FindGameObjectsWithTag("insect").Length);
 	}
 }
