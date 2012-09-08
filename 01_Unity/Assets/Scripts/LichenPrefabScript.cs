64e1e760eee97fb43b048b743bb0531f0ef137b1
Gilad Gray <giladgray@gmail.com>
2012-09-07 17:35:02 -0700

    lichen growth improved!
    grows towards tree, slowly.

--- 01_Unity/Assets/Scripts/LichenPrefabScript.cs
+++ 01_Unity/Assets/Scripts/LichenPrefabScript.cs
@@ -4,45 +4,61 @@ using System.Collections;
 public class LichenPrefabScript : MonoBehaviour {
 	
 	GameObject island;
+	GameObject tree;
 	public GameObject lichenPrefab;
-
+	public float growTime;
+	
+	float growCounter;
+	
+	bool reproducify = true;
+	
+	Vector3 spawnDirection;
+	
 	void Start () {
 		island = GameObject.Find("Island");
+		tree = GameObject.Find ("Main Tree");
+		transform.localScale = Vector3.zero;
+		
+		spawnDirection = Vector3.Normalize(tree.transform.position - transform.position + 
+			new Vector3(Random.Range (-2f, 2f),Random.Range (-2f, 2f), 0));
 	}
 	
-	void Update () {
-		
-		Reproduce();
-		
+	void FixedUpdate () {
+		if(reproducify) {
+			growCounter += Time.deltaTime;
+			transform.localScale = new Vector3 (growCounter, growCounter, growCounter / 2) / growTime;
+			// reproduce once per time period
+			if(growCounter > growTime) {
+				Reproduce();
+				growCounter = 0;
+			}
+		}
 	}
 	
 	void Reproduce () {
-		
+		reproducify = false;
 		RaycastHit hit;
 		
-		Vector3 directionToIsland = island.transform.position - transform.position;
+		// vector pointing towards tree with a bit of randomness
+		Vector3 directionToTree = tree.transform.position - transform.position + 
+			new Vector3(Random.Range (-2f, 2f),Random.Range (-2f, 2f), 0);
 		
 		// set ray start position and randomize a little
-		Vector3 lichenRayPosition = (transform.position - Vector3.Normalize(directionToIsland) * 2) + new Vector3 (Random.Range(3,6),Random.Range(3,6), 0); 
+		// at lichen position towards tree and a little above island
+		Vector3 lichenRayPosition = transform.position + Vector3.Normalize(directionToTree) + Vector3.up * 2;
 		
-		Ray lichenRay = new Ray(lichenRayPosition, directionToIsland);
-		
+		// cast a ray down through the island to find the point to spawn next lichen
+		Ray lichenRay = new Ray(lichenRayPosition, Vector3.down);
 		Physics.Raycast(lichenRay, out hit);
 		Debug.DrawRay(lichenRay.origin, lichenRay.direction);
-
-			
-		Vector3 spawnPos;
 		
+		print ("lichen ray hits " + hit.collider.tag);
+		
+		// if we hit the island then spawn a lichen there, otherwise try again.
 		if (hit.collider.tag == "island") {
 			Instantiate(lichenPrefab, hit.point, Quaternion.LookRotation(hit.normal));	
+		} else {
+			reproducify = true;
 		}
-
-		if (hit.collider.tag == "lichen" && hit.collider != this.collider) {
-			print ("I hit lichen");	
-		}
-		
-//		if (hit.collider.tag == "island") {
-//				print ("I hit the island!");
-//		}
 	}
 }
